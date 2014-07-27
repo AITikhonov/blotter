AITikhonov/blotter

commit 0988be4cf179c3d40dd54b6f700e52f8fee734f4

- fix rebalancing and rebalancing demos - update roxygen docs - bump version
git-svn-id: svn://svn.r-forge.r-project.org/svnroot/blotter@1594 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

pkg/quantstrat/R/ruleSignal.R
@@ -204,13 +204,23 @@ ruleSignal <- function(mktdata=mktdata, timestamp, sigcol, sigval, orderqty=0, o
         #TODO add fancy formals matching for osFUN
         if(orderqty!='all')
         {
-            orderqty <- osFUN(strategy=strategy, data=mktdata, timestamp=timestamp, orderqty=orderqty, ordertype=ordertype, orderside=orderside, portfolio=portfolio, symbol=symbol,...=...,ruletype=ruletype, orderprice=as.numeric(orderprice))
+            orderqty <- osFUN(strategy=strategy, 
+                              data=mktdata, 
+                              timestamp=timestamp, 
+                              orderqty=orderqty, 
+                              ordertype=ordertype, 
+                              orderside=orderside, 
+                              portfolio=portfolio, 
+                              symbol=symbol,
+                              ...=...,
+                              ruletype=ruletype, 
+                              orderprice=as.numeric(orderprice))
         }
 
-        if(!is.null(orderqty) && orderqty!=0 && !is.null(orderprice)) #orderprice could have length > 1
+        if(!is.null(orderqty) && orderqty!=0 && length(orderprice) && is.numeric(orderprice[1])) #orderprice could have length > 1
         {
-#            if(orderqty != 'all' || getPosQty(portfolio, symbol, timestamp) != 0)
-#            {
+          if(orderqty != 'all' || getPosQty(portfolio, symbol, timestamp) != 0)
+             {
                 addOrder(portfolio=portfolio, 
                          symbol=symbol, 
                          timestamp=timestamp, 
@@ -228,8 +238,8 @@ ruleSignal <- function(mktdata=mktdata, timestamp, sigcol, sigval, orderqty=0, o
                          prefer=prefer, 
                          TxnFees=TxnFees,
                          label=label,
-			 time.in.force=time.in.force)
-#            }
+			                   time.in.force=time.in.force)
+            }
         }
     }
     if(sethold) hold <<- TRUE

commit 14ce63dccb937b0d8189414c2e8a3cd87940399a

- Revert bug accidentally introduced in r1594

pkg/quantstrat/R/ruleSignal.R
@@ -217,10 +217,8 @@ ruleSignal <- function(mktdata=mktdata, timestamp, sigcol, sigval, orderqty=0, o
                               orderprice=as.numeric(orderprice))
         }
 
-        if(!is.null(orderqty) && orderqty!=0 && length(orderprice) && is.numeric(orderprice[1])) #orderprice could have length > 1
+        if(!is.null(orderqty) && orderqty!=0 && length(orderprice))
         {
-          if(orderqty != 'all' || getPosQty(portfolio, symbol, timestamp) != 0)
-             {
                 addOrder(portfolio=portfolio, 
                          symbol=symbol, 
                          timestamp=timestamp, 
@@ -239,7 +237,6 @@ ruleSignal <- function(mktdata=mktdata, timestamp, sigcol, sigval, orderqty=0, o
                          TxnFees=TxnFees,
                          label=label,
 			                   time.in.force=time.in.force)
-            }
         }
     }
     if(sethold) hold <<- TRUE